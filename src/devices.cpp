d428e64cc512a85fc242c8ba2dac28e383cd82a0
max <64427810+theoctagonalt@users.noreply.github.com>
2025-07-07 23:20:08 -0400

    oops forgot to save a file

--- src/devices.cpp
+++ src/devices.cpp
@@ -1,8 +1,8 @@
 #include "main.h"
 // #include "lemlib/api.hpp"
 
-#define LONG_GOAL_MOTOR -1
-#define CENTER_GOAL_MOTOR 2
+#define LONG_MOTOR -1
+#define CENTER_MOTOR 2
 #define INTAKE_MOTOR 3
 
 //sensors: colour sensor, inertial sensor, auton selector?
@@ -11,11 +11,11 @@ pros::Controller master (pros::E_CONTROLLER_MASTER);
 
 //motors
 
-// pros::MotorGroup left_motors ({LEFT_FRONT_MOTOR, LEFT_BACK_MOTOR, LEFT_BACK_MOTOR_TOP}, pros::MotorGear::blue);
-// pros::MotorGroup right_motors ({RIGHT_FRONT_MOTOR, RIGHT_BACK_MOTOR, RIGHT_BACK_MOTOR_TOP}, pros::MotorGear::blue);
+// pros::MotorGroup left_motors ({LEFT_FRONT_MOTOR, LEFT_BACK_MOTOR, LEFT_MIDDLE_MOTOR}, pros::MotorGear::blue);
+// pros::MotorGroup right_motors ({RIGHT_FRONT_MOTOR, RIGHT_BACK_MOTOR, RIGHT_MIDDLE_MOTOR}, pros::MotorGear::blue);
 
-pros::Motor long_goal_motor (LONG_GOAL_MOTOR, pros::MotorGear::green);
-pros::Motor center_goal_motor (CENTER_GOAL_MOTOR, pros::MotorGear::green);
+pros::Motor long_motor (LONG_MOTOR, pros::MotorGear::green);
+pros::Motor center_motor (CENTER_MOTOR, pros::MotorGear::green);
 pros::Motor intake_motor (INTAKE_MOTOR, pros::MotorGear::green);
 
 //pistons
